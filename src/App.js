1cc9a177245830df4388c9707b8772bad28d3ad3
Your Name <you@example.com>
2023-03-24 10:26:32 -0400

    did the labby stuff

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import AllocationForm from './components/AllocationForm';
+//import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -15,19 +19,49 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        {
+                            // Budget component
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
+                        }
 
-                        {/* Add Remaining component here under */}        
 
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
+                        {
+                            //Remaining component
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
+                        }        
 
-                        {/* Add ExpenseItem component here under */}        
+                        {
+                            //ExpenseTotal component
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
+                        }        
+                    </div>
+                    <h3 className='mt-3'>Allocation</h3>
+                        <div className='row '>
+                            {
+                            //ExpenseList component
+                                <div className='col-sm'>
+                                    <ExpenseList />
+                                </div>
+                            }         
+                        </div>
+                              
+                    <h3 className='mt-3'>Change allocation</h3>
+                        <div className='row mt-3'>
+                            {
+                                //AllocationForm component
+                                <div className='col-sm'>
+                                    <AllocationForm />
+                                </div>
+                            } 
+                        </div>
 
-                        {/* Add AllocationForm component here under */}        
-
-                </div>
+                
             </div>
         </AppProvider>
     );
